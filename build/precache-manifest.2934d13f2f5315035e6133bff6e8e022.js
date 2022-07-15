dd2004437ab5ffa23655b620ecdb8fd40593c9e3
phongson92 <phongson.programmer@gmail.com>
2022-07-15 15:45:21 +0700

    upload

---
+++ build/precache-manifest.2934d13f2f5315035e6133bff6e8e022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a2f28d898981c56cd742ef7c1902fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3592b7abc7423dd01eb7",
+    "url": "/static/css/main.9dad37d3.chunk.css"
+  },
+  {
+    "revision": "f903365ad1b274e23574",
+    "url": "/static/js/2.776b8aaf.chunk.js"
+  },
+  {
+    "revision": "a1921694f88d68b5e5f6260c9f21d92f",
+    "url": "/static/js/2.776b8aaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3592b7abc7423dd01eb7",
+    "url": "/static/js/main.47adf197.chunk.js"
+  },
+  {
+    "revision": "ad723c6b94fa9333cc5f",
+    "url": "/static/js/runtime-main.33b04fa7.js"
+  },
+  {
+    "revision": "680982e6aa58367e124d460071324e6f",
+    "url": "/static/media/naafi_photo.680982e6.png"
+  }
+]);
---
+++ src/contents/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">About Me</h1>
+            <h4>Hey there,</h4>
+            <h1>I'm Son Baby Cute Phomaique</h1>
+            <h3>DevOps Engineer | System <u>Administrator</u> | Wordpress <u>Designer</u></h3>
+            <br></br>
+            <p>I have 5 years of experience in System Administration and Devops Engineer.
+            </p>
+            
+            </div>
+            )
+        }
+    }
+    
+export default About
+    
